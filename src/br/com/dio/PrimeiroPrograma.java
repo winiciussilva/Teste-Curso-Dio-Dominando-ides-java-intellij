8574851c411f9b529ec64694eca7f49d953ee73b
winic <winiciusrpm@gmail.com>
2022-05-22 21:50:56 -0300

    First Commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.printf("Hello World!" + (a+b));*/
+    }
+}
